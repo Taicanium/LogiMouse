1b3304ecb6921e0082225af36bc129b85d3409b1
Quibilia <SirLevalioDaBurnshire@gmail.com>
2017-03-13 17:36:07 -0500

    Reorganization
    I've decided to go ahead and publish the Visual Studio files for this
    project. Perhaps it will ease compilation of the source.

---
+++ stdafx.h
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+#include <kernelspecs.h>
+
+#include <iostream>
+#include <stdio.h>
+
+#pragma comment (lib, "winmm.lib")
+#pragma comment (lib, "user32.lib")
+
+#include <joystickapi.h>
